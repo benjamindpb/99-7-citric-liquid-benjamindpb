42a7c3629c1045cc986fbe19f624bdf928142e3f
benjamindpb <benjamin.delpino@ug.uchile.cl>
2020-04-29 20:23:30 -0400

    se implementan las Wild Units

---
+++ src/main/java/com/github/cc3002/citricjuice/model/units/WildUnit.java
@@ -0,0 +1,7 @@
+package com.github.cc3002.citricjuice.model.units;
+
+public class WildUnit extends AbstractUnit {
+    public WildUnit(String name, int hp, int atk, int def, int evd) {
+        super(name, hp, atk, def, evd);
+    }
+}
