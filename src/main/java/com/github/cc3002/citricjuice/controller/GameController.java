f728f73ade56ec528652012740b0bd0affc954f1
benjamindpb <benjamin.delpino@ug.uchile.cl>
2020-05-31 19:06:46 -0400

    se crea y modifica clase controller

--- src/main/java/com/github/cc3002/citricjuice/controller/GameController.java
+++ src/main/java/com/github/cc3002/citricjuice/controller/GameController.java
@@ -1,6 +1,11 @@
 package com.github.cc3002.citricjuice.controller;
 
 import com.github.cc3002.citricjuice.model.board.*;
+import com.github.cc3002.citricjuice.model.unit.Player;
+import com.github.cc3002.citricjuice.model.unit.boss.BossUnit;
+import com.github.cc3002.citricjuice.model.unit.wild.WildUnit;
+
+import java.util.List;
 
 public class GameController {
     public BonusPanel createBonusPanel(int id) {
@@ -20,4 +25,15 @@ public class GameController {
 
     public NeutralPanel createNeutralPanel(int id) {
     }
+
+    public Player createPlayer(String name, int hitPoints, int attack, int defense, int evasion, IPanel panel) {
+    }
+
+    public WildUnit createWildUnit(String name, int hitPoints, int attack, int defense, int evasion) {
+    }
+
+    public BossUnit createBossUnit(String name, int hitPoints, int attack, int defense,
+                                   int evasion) {
+    }
+
 }
